98b62d1bd6e588bec03df0487c37397d5670c696
Andrew Danger Lyon <orthecreedence@gmail.com>
2013-09-03 01:45:55 -0700

    arrrg no shit

--- webroot/js/turtl.js
+++ webroot/js/turtl.js
@@ -1,6 +1,6 @@
 window.addEvent('domready', function() {
 	// add a listener for chrome ext clicks
-	$(document.body).addEvent('click:relay(div.button.chrome > a', function(e) {
+	$(document.body).addEvent('click:relay(div.button.chrome > a)', function(e) {
 		if(!chrome.webstore) return;
 		if(e) e.stop();
 		chrome.webstore.install();
