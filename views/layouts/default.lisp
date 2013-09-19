c41ce8cb924327473ff63f8e711e0e023b3e4b06
Andrew Danger Lyon <orthecreedence@gmail.com>
2013-09-19 09:14:58 -0700

    adding https piwik

--- views/layouts/default.lisp
+++ views/layouts/default.lisp
@@ -40,18 +40,15 @@
       (:script
         "
   var _paq = _paq || [];
-  _paq.push([\"trackPageView\"]);
-  _paq.push([\"enableLinkTracking\"]);
-
-  /*
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
   (function() {
-    var u=((\"https:\" == document.location.protocol) ? \"https\" : \"http\") + \"://stats.killtheradio.net/\";
-    _paq.push([\"setTrackerUrl\", u+\"piwik.php\"]);
-    _paq.push([\"setSiteId\", \"8\"]);
-    var d=document, g=d.createElement(\"script\"), s=d.getElementsByTagName(\"script\")[0]; g.type=\"text/javascript\";
-    g.defer=true; g.async=true; g.src=u+\"piwik.js\"; s.parentNode.insertBefore(g,s);
+    var u=((\"https:\" == document.location.protocol) ? \"https\" : \"http\") + \"://turtl.it/piwik//\";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 1]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
+    g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();
-  */
         "))))
 
 
