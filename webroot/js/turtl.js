59c189129a59d583e84b6cd348dd97cbdc4380ed
Andrew Danger Lyon <orthecreedence@gmail.com>
2013-09-25 10:23:47 -0700

    show/hide the right addon button

--- webroot/js/turtl.js
+++ webroot/js/turtl.js
@@ -1,9 +1,25 @@
 window.addEvent('domready', function() {
+	var using_chrome	=	!!(window.chrome && window.chrome.webstore);
+
 	// add a listener for chrome ext clicks
 	$(document.body).addEvent('click:relay(div.button.chrome > a)', function(e) {
-		if(!chrome.webstore) return;
+		if(!using_chrome) return;
 		if(e) e.stop();
 		chrome.webstore.install();
 	});
+
+	// show/hide the right button
+	var buttons	=	document.getElement('.button-row');
+	if(buttons)
+	{
+		buttons.getElements('div.button').each(function(el) {
+			if(	(el.hasClass('chrome') && using_chrome) ||
+				(el.hasClass('firefox') && !using_chrome) )
+			{
+				return false;
+			}
+			el.dispose();
+		});
+	}
 });
 
